8fd43142df2b2573bdacaf37adfb428d53a1d834
Muxen1 <max.anerud@gmail.com>
2026-01-16 11:05:37 +0100

    tid

--- script.js
+++ script.js
@@ -3,36 +3,36 @@ const music = document.getElementById('Musik');
 const muteButton = document.getElementById('muteButton');
 
 
-muteButton.addEventListener('click', function() {
-    
-    music.muted = !music.muted; 
+muteButton.addEventListener('click', function () {
 
-   
-    if (music.muted) {
-        muteButton.textContent = 'Unmute';
-    } else {
-        muteButton.textContent = 'Mute';
-    }
+  music.muted = !music.muted;
+
+
+  if (music.muted) {
+    muteButton.textContent = 'Unmute';
+  } else {
+    muteButton.textContent = 'Mute';
+  }
 
 });
 
-var countDownDate = new Date("Dec 24, 2025 00:00:00").getTime();
-      var x = setInterval(function () {
-        var now = new Date().getTime();
-        var distance = countDownDate - now;
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor(
-          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-        );
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        document.getElementById("Countdown").innerHTML =
-          days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-        if (distance < 0) {
-          clearInterval(x);
-          document.getElementById("Countdown").innerHTML = "EXPIRED";
-        }
-      }, 1000);
+var countDownDate = new Date("Dec 24, 2026 00:00:00").getTime();
+var x = setInterval(function () {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  document.getElementById("Countdown").innerHTML =
+    days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("Countdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
 
 const snowflakes = ["❄", "❅", "❆"];
 
